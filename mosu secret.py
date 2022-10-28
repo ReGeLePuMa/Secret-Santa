5f5d9b94a01431e59f092b3aefe18c6520bc7e17
ReGeLePuMa <93268175+ReGeLePuMa@users.noreply.github.com>
2022-10-28 22:52:24 +0300

    Added script file

---
+++ mosu secret.py
@@ -0,0 +1,36 @@
+import random
+import smtplib
+from email import message
+
+sender="" # Include sender's email
+sender_pass="" # Include sender's email password/ token for app use
+session=smtplib.SMTP_SSL('smtp.gmail.com', 465)
+session.login(sender,sender_pass)
+
+dict={
+    """To add items in format {Name}:{Email address}"""
+}
+
+subject="Secret Santa!"
+body="Draga {},\nPersoana careia trebuie sai dai cadou este {}.\nSemnat, spiridusul lui Mos Craciun."
+
+message="Subject:"+subject+"\n\n"+body
+
+destinatar=list(dict.keys())
+recipient=list(dict.keys())
+random.shuffle(recipient)
+i=0
+while True:
+    if destinatar[i]==recipient[i]:
+        random.shuffle(recipient)
+        i=0
+    else: 
+        i=i+1    
+    if i==len(destinatar):
+        break
+i=0
+for nume in recipient:
+    session.sendmail(sender,str(dict[nume]),message.format(str(nume),str(destinatar[i])))
+    i=i+1
+
+session.quit()    
